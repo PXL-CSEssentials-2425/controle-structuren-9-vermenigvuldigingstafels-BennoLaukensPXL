8f4893f4a016372ee062eed4d14574fc214f650d
BennoLaukensPXL <benno.laukens@student.pxl.be>
2024-11-05 13:09:53 +0100

    9Vermenigvuldigingstafel without background

---
+++ 9Vermenigvuldigingstafel/9Vermenigvuldigingstafel/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _9Vermenigvuldigingstafel
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        StringBuilder sb = new StringBuilder();
+        private void startButton_Click(object sender, RoutedEventArgs e)
+        {
+            sb.Clear();
+            int result;
+            sb.AppendLine($"De tafel van vermenigvuldiging:");
+            sb.AppendLine();
+
+            for (int table = 1; table <= 12; table ++)
+            {
+                for (int number = 1; number <= 12; number++)
+                {
+                    result = table * number;
+                    sb.AppendLine($"{table} * {number} = {result}");
+                }
+            }
+            resultTextBox.Text = sb.ToString();
+        }
+
+        private void clearButton_Click(object sender, RoutedEventArgs e)
+        {
+            resultTextBox.Clear();
+        }
+
+        private void closeButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
